11ff7f82bf2cf0759709e01b1523d5727c107a60
elly <elly.chan.mda@gmail.com>
2019-01-12 16:15:09 +0000

    design authenticated root

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,24 @@
 Rails.application.routes.draw do
   get 'home/index'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'home/index' 
+  devise_scope :user do
+    
+    authenticated :user do
+      root 'users#index', as: :authenticated_root
+    end
 
-  root 'home#index'
+    unauthenticated do
+      root 'home#index', as: :unauthenticated_root
+    end
 
-  resources :users
+    resources :users
+
+    root 'home#index'
+    
+  end
+
 
 end
