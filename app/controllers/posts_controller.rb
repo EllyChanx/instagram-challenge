ba50fb36cc343b1fd408edd8cf2ce26b600598f0
elly <elly.chan.mda@gmail.com>
2019-01-13 02:53:03 +0000

    add post create, test

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,4 +5,20 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def create
+    @post = Post.new(post_params.merge(user_id: current_user.id))
+    if @post.save
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:notice] = "Post can not be empty, please enter content."
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -3,10 +3,32 @@ require 'spec_helper'
 
 RSpec.describe PostsController, type: :controller do
 
+  before(:each) do
+    request.env['HTTP_REFERER'] = root_url
+    @user = User.create!(email: 'test@test.com', password: 'testtest')
+    allow(controller).to receive(:current_user) { instance_double('User', :id => @user.id) }
+  end
+
   describe 'GET #index' do
     it 'returns http success' do
       get :index
       expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'PUT #create' do
+    it 'assigns a newly created posts as @post' do
+      post :create, :params => {:post => {content: 'content' } }
+      expect(assigns(:post)).to be_a(Post)
+      expect(assigns(:post)).to be_persisted
+    end
+  end
+
+  describe 'PUT #create - flash notice' do
+    it 'post handle empty content and flash' do
+      post :create, :params => {:post => {description: ''} }
+      expect(controller).to set_flash[:notice]
+      expect(response).to redirect_to root_url
     end
   end
 
